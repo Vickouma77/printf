477a3f0a0563158e003901346ead8d5e7ff5b955
vickouma77 <vickprestone20@gmail.com>
2022-10-20 06:50:57 -0700

    modified preprocessor directives

--- _printf.c
+++ _printf.c
@@ -1,8 +1,6 @@
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 #include <stddef.h>
+
 /**
  * _printf - recreates the printf function
  * @format: string with format specifier
